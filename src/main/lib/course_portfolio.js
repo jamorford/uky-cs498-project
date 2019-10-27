c8b64793d5663b974aa00036bfc3be5a578937d6
David Stilz <dastilz7@gmail.com>
2019-10-27 02:15:03 -0400

    New Course Submission Implementation
    - Added implementation for a new course
    - Randomly generates a subset of student indexes for artifacts
    
    Implements:
    #5
    #10

--- src/main/lib/course_portfolio.js
+++ src/main/lib/course_portfolio.js
@@ -1,4 +1,6 @@
 const Portfolio = require('../models/CoursePortfolio')
+const Course = require('../models/Course')
+const CoursePortfolioStudentLearningOutcome = require('../Models/CoursePortfolio/StudentLearningOutcome')
 
 module.exports.new = async ({
 	department_id,
@@ -11,9 +13,122 @@ module.exports.new = async ({
 	section
 }) => {
 	// TODO
-	return {
-		id: 'todo'
-	};
+
+	// Query info for the inputted course
+	course_query = {
+		department_id: parseInt(department_id),
+		number: parseInt(course_number)
+	}
+
+	// Check if course already exists
+	course_exist = await Course
+		.query()
+		.where('department_id', department_id)
+		.where('number', course_number)
+	
+	
+	// If course doesn't exist, make a new course
+	if (course_exist.length == 0){		
+		course_result = await Course.query().insert(course_query)
+		course_id = course_result.id
+	} else {
+		course_id = course_exist[0].id
+	}
+
+	// Get student learning outcomes from department
+	// student_learning_outcomes = await (await Department.query().findById(department_id))
+	// 	.$relatedQuery('student_learning_outcomes')
+
+	// Query info for a new portfolio
+	portfolio_query = {
+		course_id: parseInt(course_id),
+		instructor_id: parseInt(instructor),
+		semester_term_id: parseInt(semester),
+		year: parseInt(year),
+		num_students: parseInt(num_students),
+		section: parseInt(section)
+	}
+
+	
+
+
+	// Check if portfolio already exists
+	portfolio_exist = await Portfolio
+		.query()
+		.where('course_id', course_id)
+		.where('semester_term_id', semester)
+		.where('section', section)
+		.where('year', year)
+
+	// If portfolio doesn't exist, make another one
+	if (portfolio_exist.length == 0) {
+		portfolio_result = await Portfolio.query().insert(portfolio_query)
+
+		console.log(student_learning_outcomes)
+
+		// Make a query to connect the portfolio to the chosen SLO
+		portfolio_slo_query = {
+			portfolio_id: parseInt(portfolio_result.id),
+			slo_id: parseInt(student_learning_outcomes[0])
+		}
+
+		console.log('portfolio slo query')
+		console.log(portfolio_slo_query)
+
+		await CoursePortfolioStudentLearningOutcome.query().insert(portfolio_slo_query)
+
+		portfolio_id = portfolio_result.id
+	} else {
+		portfolio_id = portfolio_exist[0].id
+	}
+
+	console.log('portfolio id')
+	console.log(portfolio_id)
+	
+	let raw_portfolio = await Portfolio.query()
+	.eager({
+		course: {
+			department: true
+		},
+		instructor: true,
+		semester: true,
+		outcomes: {
+			slo: {
+				metrics: true
+			},
+			artifacts: {
+				evaluations: true
+			}
+		}
+	})
+	.findById(portfolio_result.id)
+
+	console.log(raw_portfolio)
+
+	let portfolio = {
+		portfolio_id: raw_portfolio.id,
+		course_id: raw_portfolio.course_id,
+		instructor: raw_portfolio.instructor,
+		num_students: raw_portfolio.num_students,
+		outcomes: [],
+		course: {
+			department: raw_portfolio.course.department.identifier,
+			number: raw_portfolio.course.number,
+			section: raw_portfolio.section,
+			semester: raw_portfolio.semester.value,
+			year: raw_portfolio.year
+		},
+	}
+
+	for (let i in raw_portfolio.outcomes) {
+		portfolio.outcomes.push(Object.assign({
+			artifacts: raw_portfolio.outcomes[i].artifacts
+		}, raw_portfolio.outcomes[i].slo))
+	}
+
+	console.log(portfolio)
+
+	return portfolio
 }
 
 
@@ -56,6 +171,8 @@ module.exports.get = async (portfolio_id) => {
 			artifacts: raw_portfolio.outcomes[i].artifacts
 		}, raw_portfolio.outcomes[i].slo))
 	}
+
+	console.log(portfolio)
 
 	return portfolio
 }
--- src/main/routes/course.js
+++ src/main/routes/course.js
@@ -7,73 +7,75 @@ var router = express.Router();
 const Department = require('../models/Department')
 const TermType = require('../models/TermType')
 
-const course_manage_page = async (res, course_id) => {
-	let course_info = {
-		student_learning_outcomes: [
-			{
-				index: 1,
-				description: 'n/a',
-				metrics: [
-					{
-						name: 'n/a',
-						exceeds: 'n/a',
-						meets: 'n/a',
-						partially: 'n/a',
-						not: 'n/a'
-					},
-					{
-						name: 'n/a',
-						exceeds: 'n/a',
-						meets: 'n/a',
-						partially: 'n/a',
-						not: 'n/a'
-					},
-					{
-						name: 'n/a',
-						exceeds: 'n/a',
-						meets: 'n/a',
-						partially: 'n/a',
-						not: 'n/a'
-					},
-					{
-						name: 'n/a',
-						exceeds: 'n/a',
-						meets: 'n/a',
-						partially: 'n/a',
-						not: 'n/a'
-					},
-				],
-				artifacts: [
-					{
-						name: 'n/a',
-						evaluations: [
-							{
-								index: 1,
-								evaluation: [
-									{
-										metric: 1,
-										value: 6
-									},
-									{
-										metric: 2,
-										value: 6
-									},
-									{
-										metric: 3,
-										value: 6
-									},
-									{
-										metric: 4,
-										value: 6
-									}
-								]
-							}
-						]
-					}
-				]
-			}
-		]
-	};
+const course_manage_page = async (res, course_portfolio_id) => {
+	// let course_info = {
+	// 	student_learning_outcomes: [
+	// 		{
+	// 			index: 1,
+	// 			description: 'n/a',
+	// 			metrics: [
+	// 				{
+	// 					name: 'n/a',
+	// 					exceeds: 'n/a',
+	// 					meets: 'n/a',
+	// 					partially: 'n/a',
+	// 					not: 'n/a'
+	// 				},
+	// 				{
+	// 					name: 'n/a',
+	// 					exceeds: 'n/a',
+	// 					meets: 'n/a',
+	// 					partially: 'n/a',
+	// 					not: 'n/a'
+	// 				},
+	// 				{
+	// 					name: 'n/a',
+	// 					exceeds: 'n/a',
+	// 					meets: 'n/a',
+	// 					partially: 'n/a',
+	// 					not: 'n/a'
+	// 				},
+	// 				{
+	// 					name: 'n/a',
+	// 					exceeds: 'n/a',
+	// 					meets: 'n/a',
+	// 					partially: 'n/a',
+	// 					not: 'n/a'
+	// 				},
+	// 			],
+	// 			artifacts: [
+	// 				{
+	// 					name: 'n/a',
+	// 					evaluations: [
+	// 						{
+	// 							index: 1,
+	// 							evaluation: [
+	// 								{
+	// 									metric: 1,
+	// 									value: 6
+	// 								},
+	// 								{
+	// 									metric: 2,
+	// 									value: 6
+	// 								},
+	// 								{
+	// 									metric: 3,
+	// 									value: 6
+	// 								},
+	// 								{
+	// 									metric: 4,
+	// 									value: 6
+	// 								}
+	// 							]
+	// 						}
+	// 					]
+	// 				}
+	// 			]
+	// 		}
+	// 	]
+	// };
+
+	let course_info = await course_portfolio_lib.get(course_portfolio_id)
 
 	res.render('base_template', {
 		title: 'CS498 Course Portfolio',
