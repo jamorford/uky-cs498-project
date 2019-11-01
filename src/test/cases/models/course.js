70f3bc9822f857c529339b5eda4921c43326a802
David Stilz <dastilz7@gmail.com>
2019-10-31 23:36:58 -0400

    Update course.js
    Revert changes from course model test back to original

--- src/test/cases/models/course.js
+++ src/test/cases/models/course.js
@@ -1,5 +1,4 @@
 const Course = require('../../../main/models/Course')
-const Department = require('../../../main/models/Department')
 const { expect } = require('../../chai')
 
 describe('Model - Course', () => {
@@ -7,17 +6,14 @@ describe('Model - Course', () => {
 	describe('lookup', () => {
 
 		it('with id', async () => {
-			let department_result = await Department
-			.query()
-			.where("name", "Computer Science")
+			const slo = await Course.query()
+				.findById(1)
 
-			const slo = await Course
-			.query()
-			.where("department_id", department_result[0].id)
-			.where("number", 498)
-
-			expect(slo[0].department_id).to.equal(department_result[0].id)
-			expect(slo[0].number).to.equal(498)
+			expect(slo).to.deep.equal({
+				id: 1,
+				department_id: 1,
+				number: 498,
+			})
 		})
 
 	})
