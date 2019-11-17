d5a23f2516ce07a667c27b5dfef2bf13849d1ea7
David Stilz <dastilz7@gmail.com>
2019-11-17 17:49:15 -0500

    Implementation and Test Cases for Random Students Index Subset Generation
    - Implemented test cases to generate random student indexes with a parameter for amount of students to generate
    - Implemented test cases to achieve 100% code coverage
    - Test cases include testing for unique student indexes, and generating the correct amount of student indexes based on requirements
    - Implements #10, see this story for details

---
+++ src/test/cases/lib/random_students_subset.js
@@ -0,0 +1,60 @@
+const { expect } = require('../../chai')
+const random_students_subset = require('../../../main/lib/random_students_subset')
+
+describe('generates random student indexes', function () {
+
+    it ('with 5 students given 5 students', function() {				
+        // Arrange
+        let student_indexes = []
+
+        // Act
+        student_indexes = random_students_subset.generateRandomStudentIndexes(5)
+        
+        // Assert
+        expect(student_indexes.length).to.equal(5)
+    })
+
+    
+    it ('with 10 students given 10 students', function() {
+        // Arrange
+        let student_indexes = []
+
+        // Act
+        student_indexes = random_students_subset.generateRandomStudentIndexes(50)
+        
+        // Assert
+        expect(student_indexes.length).to.equal(10)
+    })
+    
+    it ('with 20 students given 100 students', function() {				
+        // Arrange
+        let student_indexes = []
+
+        // Act
+        student_indexes = random_students_subset.generateRandomStudentIndexes(100)
+        
+        // Assert
+        expect(student_indexes.length).to.equal(20)
+    })
+
+    // Make sure all student indexes are unique
+    it ('with unique student indexes', function() {
+        // Arrange
+        let scanned_student_indexes = []
+        let allUnique = true
+
+        // Act
+        let student_indexes = random_students_subset.generateRandomStudentIndexes(50)
+
+        // Assert
+        for(let i=0; i<student_indexes.length; i++){
+            if (scanned_student_indexes.includes(student_indexes[i])){
+                allUnique = false
+                i = student_indexes.length
+            } else {
+                scanned_student_indexes.push(student_indexes[i])
+            }
+        }
+        expect(allUnique).to.equal(true)
+    })
+}) 
